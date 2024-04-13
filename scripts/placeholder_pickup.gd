e780457b9fe524d33923072383fbe7dd070c3ced
Faisal Alkubaisi <faisal@spacecrescent.com>
2024-04-13 17:22:54 +0300

    fixes tiles and singleton issues.
    Fixed issues in tiles and singleton.

--- scripts/placeholder_pickup.gd
+++ scripts/placeholder_pickup.gd
@@ -1,16 +1,17 @@
 extends Area2D
 
+signal collected
 
 func _ready():
-    #hint Autoload (GroupNames)
-    add_to_group(GroupNames.PickupGroup)
-    var scene = get_tree().current_scene
-    if scene.has_method("check_if_win"):
-        connect("body_entered", Callable(scene, "check_if_win"))
-        
-        
+	#hint Autoload (GroupNames)
+	add_to_group(GroupNames.PickupGroup)
+	var scene = get_tree().current_scene
+	if scene.has_method("check_if_win"):
+		connect("collected", Callable(scene, "check_if_win"))
+		
+		
 func _on_body_entered(body):
-    if body.is_in_group(GroupNames.PlayerGroup):
-        emit_signal("body_entered")
-        call_deferred("queue_free")
+	if body.is_in_group(GroupNames.PlayerGroup):
+		emit_signal("collected")
+		call_deferred("queue_free")
 
